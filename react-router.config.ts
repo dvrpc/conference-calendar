9e2861700c6948411d3537a83fa047f279773ce3
Jesse Stranahan <jesse.stranahan@gmail.com>
2026-08-17 19:51:38 +0000

    fix: add allowedActionOrigins to fix CSRF check failing behind reverse proxy

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: true,
   basename: import.meta.env.VITE_BASE,
+  allowedActionOrigins: ["cloud.dvrpc.org"],
 } satisfies Config;
